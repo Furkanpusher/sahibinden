6eaa133640d9e04fa306de608deda33a758a503b
Furkan Şenol <ffurkan.senol@gmail.com>
2026-08-13 11:09:08 +0300

    Serializers Fixed.

--- listings/serializers.py
+++ listings/serializers.py
@@ -5,12 +5,12 @@ from rest_framework import serializers
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = ['id', 'title', 'location', 'city', 'district', 'price', 'listing_owner', 'listing_date', 'listing_update']
+        fields = ['id', 'title', 'city', 'district', 'price', 'listing_owner', 'listing_date', 'listing_update']
 
 class CarListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = CarListing
-        fields = ['id', 'title', 'location', 'city', 'district', 'price', 'listing_owner', 'listing_date',
+        fields = ['id', 'title', 'city', 'district', 'price', 'listing_owner', 'listing_date',
                 'brand', 'model', 'year', 'transmission_type', 'km', 'fuel_type', 'body_type',
                   'color', 'engine_size', 'engine_power', 'traction', 'status', 'avg_fuel_consumption',
                 'fuel_tank', 'changed_parts', 'from_whom', 'tramer']
@@ -19,6 +19,6 @@ class CarListingSerializer(serializers.ModelSerializer):
 class HouseListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = HouseListing
-        fields = ['id', 'title', 'location', 'city', 'district', 'price', 'listing_owner', 'listing_date',
+        fields = ['id', 'title', 'city', 'district', 'price', 'listing_owner', 'listing_date',
                   'meter_squared', 'building_aged', 'number_of_floors', 'number_of_rooms',
                   'floor', 'credit_eligibility']
